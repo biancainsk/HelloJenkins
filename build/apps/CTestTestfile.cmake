2cd8ebed49d4e5596edebc46314537003a593cc5
Bianca <bianca.ionescu@roinspace.com>
2022-12-14 15:25:40 +0200

    Test commit

--- build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Bianca/HelloJenkins/apps
-# Build directory: /mnt/c/Users/Bianca/HelloJenkins/build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
