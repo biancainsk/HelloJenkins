6013d0416adc26ca626684e6fcb86e75855b67cc
Bianca <bianca.ionescu@roinspace.com>
2022-12-18 22:19:45 +0200

    Changed jenkinsfile

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-apt-get update && apt-get install git && apt-get install cmake gcc
-apt-get update && apt-get install build-essential
-RUN apt-get update && apt-get install ca-certificates -y
 
 mkdir build
 cd build
